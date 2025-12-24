93a4c82c16652d48f6855574eec9377bfe9cf900
mmarianneasis <marianneasis.work@gmail.com>
2025-12-23 20:47:56 -0500

    Preloader

--- js/custom.js
+++ js/custom.js
@@ -2,31 +2,47 @@
 
   "use strict";
 
-    // COLOR MODE
-    $('.color-mode').click(function(){
-        $('.color-mode-icon').toggleClass('active')
-        $('body').toggleClass('dark-mode')
-    })
+  //PRELOADER
+  $(window).on("load", function () {
+    const $preloader = $(".preloader");
 
-    // PROJECT CAROUSEL
-    $('.owl-carousel').owlCarousel({
-    	items: 1,
-	    loop:true,
-	    margin:10
-	});
-
-    // SMOOTHSCROLL
-    $(function() {
-      $('.nav-link, .custom-btn-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
+    setTimeout(function () {
+      $preloader.css({
+        opacity: "0",
+        transition: "opacity 0.5s ease"
       });
-    });  
 
-    // TOOLTIP
-    $('.social-links a').tooltip();
+      setTimeout(function () {
+        $preloader.remove();
+      }, 500);
+    }, 5000); // matches animation duration
+  });
+
+  // COLOR MODE
+  $('.color-mode').click(function(){
+      $('.color-mode-icon').toggleClass('active')
+      $('body').toggleClass('dark-mode')
+  })
+
+  // PROJECT CAROUSEL
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop:true,
+    margin:10
+});
+
+  // SMOOTHSCROLL
+  $(function() {
+    $('.nav-link, .custom-btn-link').on('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });  
+
+  // TOOLTIP
+  $('.social-links a').tooltip();
 
 })(jQuery);
